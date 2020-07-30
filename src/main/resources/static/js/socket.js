f74798e839ded70dbfc1a31ffd3e3eb5daad0b0d
ghostxbh <ghostxbh@hotmail.com>
2020-07-30 17:15:54 +0800

    modify socket

--- src/main/resources/static/js/socket.js
+++ src/main/resources/static/js/socket.js
@@ -1,7 +1,8 @@
 /**
  * Create by ghostxbh 2020/7/28
  */
-(function() {
+layui.define(['element', 'layer', 'form'], function (exports) {
+    var $ = layui.jquery;
     var host = window.document.location.host;
     var pathName = window.document.location.pathname;
     var projectName = pathName.substring(0, pathName.substr(1).indexOf('/') + 1);
@@ -9,28 +10,30 @@
 
     var webSocket = null;
     if ('WebSocket' in window || 'MozWebSocket' in window) {
-        webSocket = new WebSocket(wsServer+"/systemInfoSocketServer");
+        webSocket = new WebSocket(wsServer + "/systemInfoSocketServer");
     } else {
-        webSocket = new SockJS( wsServer+"/sockjs/systemInfoSocketServer");
+        webSocket = new SockJS(wsServer + "/sockjs/systemInfoSocketServer");
     }
 
-    webSocket.onerror = function(event) {
-        jp.alert("websockt连接发生错误，请刷新页面重试!")
+    webSocket.onerror = function (event) {
+        layer.msg("websockt连接发生错误，请刷新页面重试!", { icon: 5 })
     };
 
     // 接收到消息的回调方法
-    webSocket.onmessage = function(event) {
-        var res=event.data;
-        if(window.Notification && Notification.permission !== "denied") {
-            Notification.requestPermission(function(status) {
-                new Notification('系统通知', { body: res });
+    webSocket.onmessage = function (event) {
+        var res = event.data;
+        if (window.Notification && Notification.permission !== "denied") {
+            Notification.requestPermission(function (status) {
+                new Notification('系统通知', {body: res});
             });
         } else {
-            alert('系统通知: ' + res);
+            layer.msg("系统通知: " + res, { icon: 6 })
         }
+        console.log(res)
         location.href = "/notice/";
         var audio = window.document.createElement("audio");
         audio.src = "/notice/layui/css/modules/layim/voice/default.wav";
         audio.play()
     };
-})(jQuery)
+    exports('socket', {});
+});
